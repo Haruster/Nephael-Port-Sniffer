ff1aca623b057b5f3bb5b06d1eb5223176229a39
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-11 02:31:19 +0900

    Update main.rs

--- Kinesys Port Sniffer/port-sniffer/src/main.rs
+++ Kinesys Port Sniffer/port-sniffer/src/main.rs
@@ -1,6 +1,7 @@
 //Kinesys Port Sniffer.rs
 
 use std::env;
+use std::io::{self, }
 use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
@@ -64,6 +65,21 @@ impl Argumants {
 fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
     
     let mut port: u16 = start_port + 1;
+
+    loop {
+
+        match TcpStream::connect((addr, port) {
+            Ok(_) => {
+                print!(".");
+                
+                io::stdout().flush().unwrap();
+
+                tx.send(port).unwrap();
+
+            }
+        })
+
+    }
 
 }
 
